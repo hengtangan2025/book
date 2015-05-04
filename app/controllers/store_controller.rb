9611b7b3ec5844f04daba4d8df897645b26d4974
zhoujianbin <504371515@qq.com>
2015-05-04 11:53:52 +0800

    first commit

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,9 +4,6 @@ class StoreController < ApplicationController
   end
 
   def search
-    @books = Book.where(params[:title] = 'search_text')
-  end
-
-  def classify 
+    @books = Book.where(["title = ? or keyword = ? or author = ?", params[:search_text], params[:search_text], params[:search_text]])
   end
 end
